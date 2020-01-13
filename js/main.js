800c84743e73605cb66f8ccf8f7c87cecab52171
Nick Simpson <nsimpson1983@gmail.com>
2020-01-13 17:32:48 -0500

    adding updates to website

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@
 
 /* Smooth Scroll */
 $(document).ready(function() {
-  
-  var scrollLink = $('.scroll');
-  
+  var scrollLink = $(".scroll");
+
   // Smooth scrolling
   scrollLink.click(function(e) {
     e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1500 );
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top
+      },
+      1500
+    );
   });
-  
 });
 /* End Smooth Scroll */
-
 
 /* Top Navigation Sticky */
 /*$(window).on('scroll', function() {
@@ -33,32 +33,34 @@ var myIndex = 0;
 carousel();
 
 function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 /* End Slideshow */
 
-
 /* Scroll to top */
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
